33cf688aa5245e3e8872a62c7f0818058961aed4
calidion <calidion@gmail.com>
2016-03-03 01:20:41 +0800

    updated

--- test/callbacks/oauth.js
+++ test/callbacks/oauth.js
@@ -2,6 +2,8 @@ var assert = require('assert');
 var validator = require('validator');
 import settings from 'node-weixin-settings';
 
+import callbacks from '../../lib/callbacks';
+
 import conf from '../config';
 
 var req = {
@@ -30,6 +32,25 @@ settings.set(req.session.id, 'urls', conf.urls, function() {
         done();
       }
     });
-    callback(false, {});
+    callback(true, {});
+  });
+
+  it('should test oauth callback success', function(done) {
+    var oauth = require('../../lib/callbacks/oauth');
+    var json = {
+      openid: 'openid',
+      access_token: 'access_token',
+      refresh_token: 'refresh_token'
+    };
+    callbacks.oauth.success = function(request, wx) {
+      assert.deepEqual(request, req);
+      assert.equal(true, wx.openid === json.openid);
+      /*eslint camelcase: [2, {properties: "never"}]*/
+      assert.equal(true, wx.accessToken === json.access_token);
+      assert.equal(true, wx.refreshToken === json.refresh_token);
+      done();
+    };
+    var callback = oauth.success(req, {});
+    callback(false, json);
   });
 });
