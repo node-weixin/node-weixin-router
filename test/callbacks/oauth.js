c5df47f5538757080ab529954e7660aeeac00055
calidion <calidion@gmail.com>
2016-03-03 02:00:14 +0800

    updated

--- test/callbacks/oauth.js
+++ test/callbacks/oauth.js
@@ -42,15 +42,19 @@ settings.set(req.session.id, 'urls', conf.urls, function() {
       access_token: 'access_token',
       refresh_token: 'refresh_token'
     };
-    callbacks.oauth.success = function(request, wx) {
+    var res = {
+      df: 'sdfsf'
+    };
+    callbacks.oauth.success = function(request, response, wx) {
       assert.deepEqual(request, req);
+      assert.deepEqual(response, res);
       assert.equal(true, wx.openid === json.openid);
       /*eslint camelcase: [2, {properties: "never"}]*/
       assert.equal(true, wx.accessToken === json.access_token);
       assert.equal(true, wx.refreshToken === json.refresh_token);
       done();
     };
-    var callback = oauth.success(req, {});
+    var callback = oauth.success(req, res);
     callback(false, json);
   });
 });
